7d60dd327ac22b673d3c60ec224a292c1d47f69b
Quincy Le <qle@ciena.com>
2015-10-07 12:28:34 -0400

    Initial commit

---
+++ addon/pods/components/frost-text/component.js
@@ -0,0 +1,12 @@
+import Ember from "ember";
+import _ from "lodash/lodash";
+
+export default Ember.TextField.extend({
+	classNames: ["frost-text"],
+
+	oninput: Ember.on("input", function() {
+		if (_.isFunction(this.attrs["on-input"])) {
+			this.attrs["on-input"]({id: this.get("id"), value: this.get("value")});
+		}
+	})
+});
---
+++ tests/dummy/app/pods/frost-text-demo/controller.js
@@ -0,0 +1,20 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+	errored: true,
+
+	actions: {
+		text(attrs) {
+			this.notifications.addNotification({
+				message: "value: '" + attrs.value + "'",
+				type: "success",
+				autoClear: true,
+				clearDuration: 2000
+			});
+		},
+
+		toggleError() {
+			this.toggleProperty('errored');
+		}
+	}
+});
