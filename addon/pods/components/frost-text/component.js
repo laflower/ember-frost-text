deef1a5c55ba25a0c2ee61048ba4fb9ffd2744a0
Steven Glanzer <sglanzer@ciena.com>
2015-11-26 13:00:41 -0500

    Playing with positional params

--- addon/pods/components/frost-text/component.js
+++ addon/pods/components/frost-text/component.js
@@ -1,15 +1,27 @@
 import Ember from "ember";
 import _ from "lodash/lodash";
 
-export default Ember.TextField.extend({
+const FrostText = Ember.TextField.extend({
 	classNames: ["frost-text"],
+	classNameBindings: ['isDisabled:disabled'],
+	attributeBindings: [
+		'isDisabled:disabled'
+	],
+
+	isDisabled: Ember.computed('states.[]', function() {
+		return _.includes(this.get('states'), 'disabled');
+	}),
 
 	oninput: Ember.on("input", function() {
 		if (_.isFunction(this.attrs["on-input"])) {
 			this.attrs["on-input"]({id: this.get("id"), value: this.get("value")});
 		}
 	})
-}).reopenClass({
+});
+
+FrostText.reopenClass({
 	positionalParams: 'states'
 });
+
+export default FrostText;
 
