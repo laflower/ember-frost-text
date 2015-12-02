3e6e25fd23a2de3f0cd012ba08cdd617902c6533
Gosia Hyndman <mhyndman@ciena.com>
2015-12-02 14:39:14 -0500

    adding readme file based on the new frost-docs component

--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -16,5 +16,55 @@ export default Ember.Controller.extend({
 		toggleError() {
 			this.toggleProperty('error');
 		}
+	},
+	tabs: [
+		{id:'readme', title:'Readme', defaultTab:true},
+		{id:'demo', title:'Demo'}
+	],
+
+	// Readme examples
+	autofocus: {
+		alias: 'autofocus',
+		template: `
+      {{frost-text <a>autofocus</a>=<bd>true</bd>}}
+    `
+	},
+
+	disabled: {
+		alias: 'disabled',
+		template: `
+      {{frost-text <a>disabled</a>=<bd>true</bd>}}
+    `
+	},
+
+	error1: {
+		alias: 'error',
+		controller: `
+      <ba>isError</ba>: <bd>true</bd>
+    `,
+		template: `
+      {{frost-text <e>classNameBindings</e>='<ba>isError</ba>:<c>error</c>'}}
+    `
+	},
+
+	readonly: {
+		alias: 'readonly',
+		template: `
+      {{frost-text <a>readonly</a>=<bd>true</bd> <a>value</a>=<bd>'read only text'</bd>}}
+    `
+	},
+
+	oninput: {
+		alias: 'on-input',
+		template: `
+      {{frost-text <a>on-input</a>=(<e>action</e> <ac>'updatePassword'</ac>)}}
+    `,
+		controller: `
+      <e>actions</e>: {
+        <ac>updatePassword</ac>(<ad>attrs</ad>) {
+          console.log(<ad>attrs.value</ad>);
+        }
+      }
+    `
 	}
 });
