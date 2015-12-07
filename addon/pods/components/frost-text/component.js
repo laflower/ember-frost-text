a62419a9bbc50df8f78a15293aee205758dc0285
Roxanne Panthaky <rpanthak@ciena.com>
2015-12-07 16:19:38 -0500

    fixed text input detection

--- addon/pods/components/frost-text/component.js
+++ addon/pods/components/frost-text/component.js
@@ -4,7 +4,7 @@ import _ from "lodash/lodash";
 export default Ember.TextField.extend({
 	classNames: ["frost-text"],
 
-	oninput: Ember.on("input", function() {
+	oninput: Ember.on("keyUp", function(attrs) {
 		if (_.isFunction(this.attrs["on-input"])) {
 			this.attrs["on-input"]({id: this.get("id"), value: this.get("value")});
 		}
