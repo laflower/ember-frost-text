0f80c939e8390a08a30d9651f069ab0f96c943b2
Roxanne Panthaky <rpanthak@ciena.com>
2015-12-07 16:24:13 -0500

    remove attr. was added for testing

--- addon/pods/components/frost-text/component.js
+++ addon/pods/components/frost-text/component.js
@@ -4,7 +4,7 @@ import _ from "lodash/lodash";
 export default Ember.TextField.extend({
 	classNames: ["frost-text"],
 
-	oninput: Ember.on("keyUp", function(attrs) {
+	oninput: Ember.on("keyUp", function() {
 		if (_.isFunction(this.attrs["on-input"])) {
 			this.attrs["on-input"]({id: this.get("id"), value: this.get("value")});
 		}
